91859d25dc6e7f6a6e5ec09e68f7b98fc43ca6a8
wwaaoonn <69627172+wwaaoonn@users.noreply.github.com>
2022-01-01 18:52:54 +0900

    use template

--- web.go
+++ web.go
@@ -2,14 +2,37 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"text/template"
 )
 
 func main() {
 	fmt.Println("start!")
 
+	html := `<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Document</title>
+	</head>
+	<body>
+		<h1>Index</h1>
+		<p>This is sample web page.</p>
+	</body>
+	</html>`
+
+	tf, er := template.New("index").Parse(html)
+	if er != nil {
+		log.Fatal(er)
+	}
 	hh := func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello, This is GO-server!!"))
+		er = tf.Execute(w, nil)
+		if er != nil {
+			log.Fatal(er)
+		}
 	}
 
 	http.HandleFunc("/hello", hh)
